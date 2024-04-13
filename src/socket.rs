a0aad38099575e6838bd3ce798a310c3dd0382c4
John Agapeyev <jagapeyev@gmail.com>
2024-04-12 20:38:27 -0700

    Wrap the socket API

--- src/socket.rs
+++ src/socket.rs
@@ -1,4 +1,9 @@
-use libc::{sockaddr_alg, AF_ALG};
+//use libc::{sockaddr_alg, AF_ALG};
+use libc::*;
+
+use std::io;
+use std::os::fd::FromRawFd;
+use std::os::fd::OwnedFd;
 
 pub fn fill_addr_unchecked(salg_type: &[u8], salg_name: &[u8]) -> sockaddr_alg {
     sockaddr_alg {
@@ -8,4 +13,16 @@ pub fn fill_addr_unchecked(salg_type: &[u8], salg_name: &[u8]) -> sockaddr_alg {
         salg_type: <[u8; 14]>::try_from(salg_type).unwrap(),
         salg_name: <[u8; 64]>::try_from(salg_name).unwrap(),
     }
+}
+
+pub unsafe fn create_socket() -> io::Result<OwnedFd> {
+    let fd = libc::socket(AF_ALG, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0);
+    match fd {
+        -1 => Err(io::Error::last_os_error()),
+        _ => Ok(OwnedFd::from_raw_fd(fd)),
+    }
+}
+
+pub struct Socket {
+    pub fd: OwnedFd,
 }
