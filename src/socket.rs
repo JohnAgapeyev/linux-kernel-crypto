1a13987281948c550a77830ae4bf3b4239226db3
John Agapeyev <jagapeyev@gmail.com>
2024-04-12 21:00:44 -0700

    Add the bind call for the socket creation

--- src/socket.rs
+++ src/socket.rs
@@ -2,10 +2,12 @@
 use libc::*;
 
 use std::io;
+use std::mem;
+use std::os::fd::AsRawFd;
 use std::os::fd::FromRawFd;
 use std::os::fd::OwnedFd;
 
-pub fn fill_addr_unchecked(salg_type: &[u8], salg_name: &[u8]) -> sockaddr_alg {
+pub fn fill_addr(salg_type: &[u8], salg_name: &[u8]) -> sockaddr_alg {
     sockaddr_alg {
         salg_family: AF_ALG as u16,
         salg_feat: 0u32,
@@ -15,12 +17,24 @@ pub fn fill_addr_unchecked(salg_type: &[u8], salg_name: &[u8]) -> sockaddr_alg {
     }
 }
 
-pub unsafe fn create_socket() -> io::Result<OwnedFd> {
-    let fd = libc::socket(AF_ALG, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0);
-    match fd {
-        -1 => Err(io::Error::last_os_error()),
-        _ => Ok(OwnedFd::from_raw_fd(fd)),
+pub unsafe fn create_socket(salg_type: &[u8], salg_name: &[u8]) -> io::Result<OwnedFd> {
+    let sock = match libc::socket(AF_ALG, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0) {
+        -1 => panic!("{}", io::Error::last_os_error().to_string()),
+        fd => OwnedFd::from_raw_fd(fd),
+    };
+
+    let addr = fill_addr(salg_type, salg_name);
+
+    match libc::bind(
+        sock.as_raw_fd(),
+        &addr as *const sockaddr_alg as *const sockaddr,
+        mem::size_of_val(&addr).try_into().unwrap(),
+    ) {
+        -1 => return Err(io::Error::last_os_error()),
+        _ => (),
     }
+
+    Ok(sock)
 }
 
 pub struct Socket {
