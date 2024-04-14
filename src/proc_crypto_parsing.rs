8557a8ede2ece8972a146f16ef85a2e4c09a291d
John Agapeyev <jagapeyev@gmail.com>
2024-04-14 16:06:15 -0700

    Add enum for known entry keys

--- src/proc_crypto_parsing.rs
+++ src/proc_crypto_parsing.rs
@@ -1,8 +1,62 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Error, ErrorKind, Result};
 
 const CRYPTO_FILE_PATH: &str = "/proc/crypto";
+
+enum EntryKey {
+    Async,
+    BlockSize,
+    ChunkSize,
+    DigestSize,
+    Driver,
+    GenIv,
+    Internal,
+    IvSize,
+    Max,
+    MaxAuthSize,
+    Min,
+    Module,
+    Name,
+    Priority,
+    RefCnt,
+    SeedSize,
+    SelfTest,
+    StateSize,
+    Type,
+    WalkSize,
+}
+
+impl TryFrom<&str> for EntryKey {
+    type Error = std::io::Error;
+
+    fn try_from(value: &str) -> Result<Self> {
+        match value {
+            "async" => Ok(EntryKey::Async),
+            "blocksize" => Ok(EntryKey::BlockSize),
+            "chunksize" => Ok(EntryKey::ChunkSize),
+            "digestsize" => Ok(EntryKey::DigestSize),
+            "driver" => Ok(EntryKey::Driver),
+            "geniv" => Ok(EntryKey::GenIv),
+            "internal" => Ok(EntryKey::Internal),
+            "ivsize" => Ok(EntryKey::IvSize),
+            "max" => Ok(EntryKey::Max),
+            "maxauthsize" => Ok(EntryKey::MaxAuthSize),
+            "min" => Ok(EntryKey::Min),
+            "module" => Ok(EntryKey::Module),
+            "name" => Ok(EntryKey::Name),
+            "priority" => Ok(EntryKey::Priority),
+            "refcnt" => Ok(EntryKey::RefCnt),
+            "seedsize" => Ok(EntryKey::SeedSize),
+            "selftest" => Ok(EntryKey::SelfTest),
+            "statesize" => Ok(EntryKey::StateSize),
+            "type" => Ok(EntryKey::Type),
+            "walksize" => Ok(EntryKey::WalkSize),
+            _ => Err(Error::from(ErrorKind::InvalidData)),
+        }
+    }
+}
 
 fn chunk_entries(contents: impl BufRead) -> Result<Vec<Vec<String>>> {
     contents
@@ -20,6 +74,10 @@ fn trim_line(line: &str) -> Result<(&str, &str)> {
         2 => Ok((tokens[0].trim(), tokens[1].trim())),
         _ => Err(Error::from(ErrorKind::InvalidData)),
     }
+}
+
+fn parse_entries(contents: impl BufRead) -> Result<HashMap<String, HashMap<EntryKey, String>>> {
+    return Ok(HashMap::new());
 }
 
 #[cfg(test)]
